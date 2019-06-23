7b90c62254017737f6d95547dcd04d340bcd02e4
DavidYu <yu.david880@gmail.com>
2019-06-24 00:29:18 +0900

    Init

--- cos_pro_python/test01.py
+++ cos_pro_python/test01.py
@@ -1,5 +1,34 @@
-import os
+'''
+    "XS", "S", "M", "L", "XL", "XXL"
+'''
 
-print(os.listdir('./'))
+category = list(["XS", "S", "M", "L", "XL", "XXL"])
+print(category)
 
-print("Hello World!")
+required_amt = list([1, 0, 2, 3, 1, 0])
+print(required_amt)
+
+for a, b in zip(category, required_amt):
+    print(a, b)
+
+required_size = list(["XS", "S", "L", "L", "XL", "S"])
+print(required_size)
+
+answer = [0 for _ in required_size]
+print(answer)
+
+for i, v in enumerate(required_size):
+    if v == "XS":
+        answer[0] += 1
+    elif v == "S":
+        answer[1] += 1
+    elif v == "M":
+        answer[2] += 1
+    elif v == "L":
+        answer[3] += 1
+    elif v == "XL":
+        answer[4] += 1
+    elif v == "XXL":
+        answer[5] += 1
+
+print(answer)
